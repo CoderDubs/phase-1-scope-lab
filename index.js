0df3585137791093ccba905faba1e0321f595725
Collin H <cwhouar15@gmail.com>
2024-02-20 15:36:52 -0800

    scope

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+customerName = 'bob';
+const leastFavoriteCustomer = 'Collin'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'not Collin';
+}
